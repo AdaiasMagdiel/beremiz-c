a98b72845a1d47a6b215627be7b8e0b9450308f8
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-20 23:07:55 -0300

    feat: Implement REPL
    - Added a REPL to the program.
    - Improved the error handling in the REPL.
    - Added a copyright notice and a brief description of how to exit the REPL.

--- src/main.c
+++ src/main.c
@@ -2,26 +2,53 @@
 #include <stdlib.h>
 #include "lexer.h"
 
+#define MAX_LINE_LENGTH 128
+
 char *readFile(char *filepath);
 
 void runProgram(Lexer *lexer) {
 	lexer->file = "REPL";
-	printf("The REPL was not implemented yet.\n");
-	printf("File: %s\n", lexer->file);
+
+	printf("Beremiz C Version  Copyright (C) 2024 Adaías Magdiel\n"
+		   "Use Ctrl+C ou Ctrl+Z to exit.\n\n");
+
+	while (1) {
+		char buffer[MAX_LINE_LENGTH];
+
+		printf("> ");
+		char *input = fgets(buffer, sizeof(buffer), stdin);
+
+		if (input == NULL) {
+            if (feof(stdin)) {
+                break;
+            } else {
+                perror("Unable to run the REPL.\n");
+                exit(1);
+            }
+        }
+
+		lexer->content = buffer;
+		lexer->current = 0;
+		lexer->line = 1;
+		lexer->col = 1;
+
+		scan(lexer);
+	}
 }
 
 void runFile(Lexer *lexer, char *file) {
 	lexer->file = file;
 	lexer->content = readFile(file);
-	lexer->current = 0;
-	lexer->line = 1;
-	lexer->col = 1;
 
 	scan(lexer);
 }
 
 int main(int argc, char **argv) {
 	Lexer lexer = {0};
+
+	lexer.current = 0;
+	lexer.line = 1;
+	lexer.col = 1;
 
 	if (argc == 1) {
 		runProgram(&lexer);
@@ -35,7 +62,6 @@ int main(int argc, char **argv) {
 			   "                        If no file is provided, the program will start the REPL.\n");
 	}
 
-	cleanup(&lexer);
 	return 0;
 }
 
