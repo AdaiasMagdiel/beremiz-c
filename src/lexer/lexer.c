ab4dd8d26881131eea6374b712e5176fcacdf815
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-21 18:48:55 -0300

    feat: Add number token scanning
    This commit adds functionality to the lexer to scan for number tokens. This includes:
    
    - Adding a new `isNumber` function to check if a character is a number.
    - Adding a new `extractNumber` function to extract a number from the input.
    - Updating the `scan` function to recognize number tokens and create a `NUMBER` token with the extracted value.
    - Modifying the `print_tokens` function to print number tokens in the correct format.

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "lexer.h"
+#include "tokens.h"
 #include "data_extractor.h"
+#include "dynamic_array.h"
 
 int isAtEnd(Lexer *lexer) {
 	const int length = strlen(lexer->content);
@@ -35,17 +38,33 @@ int isNumber(char ch) {
 }
 
 void scan(Lexer *lexer) {
+	Array tokens;
+	array_init(&tokens, sizeof(Token));
+
 	while (!isAtEnd(lexer)) {
 		char ch = peek(lexer, 0);
 
 		if (isNumber(ch)) {
 			int value = extractNumber(lexer);
 			printf("isNumber: %d\n", value);
+
+			Loc location = {lexer->file, lexer->line, lexer->col};
+
+			Token token;
+			token.type = NUMBER;
+			token.value = malloc(sizeof(int));
+			*(int *)token.value = value;
+			token.loc = location;
+
+			array_push(&tokens, &token);
 		} else {
 			consume(lexer);
 		}
 	}
 
+	print_tokens(tokens);
+
+	tokens_cleanup(&tokens);
 	cleanup(lexer);
 }
 
