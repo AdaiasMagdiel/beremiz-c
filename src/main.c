2d278de572d7daf1b3bf133b03f84bf41f550e46
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-22 00:23:39 -0300

    feat: Add basic operations
    This commit introduces basic arithmetic operations to the calculator, specifically addition.
    
    - The `PLUS` token now pops the two top operands from the stack, performs addition, and pushes the result back onto the stack.
    - The `SHOW` token now pops the top operand from the stack and displays its value if it is a number.
    - Added basic error handling for insufficient operands in the stack for both `PLUS` and `SHOW` operations.

--- src/main.c
+++ src/main.c
@@ -46,51 +46,68 @@ void parser(Array tokens) {
 
 	for (int i=0; i<tokens.length; i++) {
 		Token *token = &((Token *)tokens.array)[i];
-		Token tk;
 
-        switch (token->type) {
-            case NUMBER:
-	            tk.type = NUMBER;
-	            tk.value = malloc(sizeof(int));
-	            *(int *)tk.value = *(int *)token->value;
-	            tk.loc = token->loc;
+		if (token->type == NUMBER) {
+			Token tk;
+            tk.type = NUMBER;
+            tk.value = malloc(sizeof(int));
+            *(int *)tk.value = *(int *)token->value;
+            tk.loc = token->loc;
 
-	            array_push(&stack, &tk);
+            array_push(&stack, &tk);
 
-                break;
+		} else if (token->type == PLUS) {
+			// ERROR
+        	if (stack.length < 2) {
+                fprintf(stderr, "Not enough operands in the stack for PLUS operation.\n");
 
-            case PLUS:
-            	// ERROR
-            	if (stack.length < 2) {
-	                fprintf(stderr, "Not enough operands in the stack for PLUS operation\n");
+                tokens_array_cleanup(&tokens);
+				tokens_array_cleanup(&stack);
 
-	                tokens_array_cleanup(&tokens);
-					tokens_array_cleanup(&stack);
+                exit(EXIT_FAILURE);
+            }
 
-	                exit(EXIT_FAILURE);
-	            }
+            Token b;
+            array_pop(&stack, &b);
 
-                Token b;
-                array_pop(&stack, &b);
+            Token a;
+            array_pop(&stack, &a);
 
-                Token a;
-                array_pop(&stack, &a);
+            Token tk;
+            tk.type = NUMBER;
+            tk.value = malloc(sizeof(int));
+            *(int *)tk.value = *(int *)a.value + *(int *)b.value;
+            tk.loc = token->loc;
 
-                tk.type = NUMBER;
-	            tk.value = malloc(sizeof(int));
-	            *(int *)tk.value = *(int *)a.value + *(int *)b.value;
-	            tk.loc = token->loc;
+            array_push(&stack, &tk);
 
-	            array_push(&stack, &tk);
+            free(a.value);
+            free(b.value);
 
-	            free(a.value);
-	            free(b.value);
+        } else if (token->type == SHOW) {
+        	// ERROR
+        	if (stack.length < 1) {
+                fprintf(stderr, "Not enough operands in the stack for SHOW operation.\n");
 
-                break;
-            default:
-                printf("Not Implemented.\n");
-                break;
-        }
+                tokens_array_cleanup(&tokens);
+				tokens_array_cleanup(&stack);
+
+                exit(EXIT_FAILURE);
+            }
+
+            Token value;
+            array_pop(&stack, &value);
+
+            if (value.type == NUMBER) {
+            	printf("%d\n", *(int *)value.value);
+
+            } else {
+            	printf("Not implemented type `%s` for `SHOW` operation.\n", token_type_to_str(token->type));
+            }
+
+		} else {
+			printf("Not Implemented in `parser`.\n");
+		}
 	}
 
 	print_tokens(stack);
