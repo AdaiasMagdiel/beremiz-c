90299ca2f74ba5bbb9fce37a27ff6d57a9fd37a3
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-22 00:24:11 -0300

    feat: Add support for SHOW token
    - Added a new SHOW token type.
    - Implemented printing support for the new SHOW token.
    - Created a new `token_type_to_str` function to convert token types to strings.
    - Updated the `print_tokens` function to print the SHOW token type and value.

--- src/tokens/tokens.c
+++ src/tokens/tokens.c
@@ -13,15 +13,10 @@ void tokens_array_cleanup(Array *tokens) {
 }
 
 void print_tokens(Array tokens) {
-    const char *types[] = {
-        "NUMBER",
-        "PLUS"
-    };
-
 	for (int i=0; i<tokens.length; i++) {
 		Token *token = &((Token *)tokens.array)[i];
 
-        printf("Type: %s", types[token->type]);
+        printf("Type: %s", token_type_to_str(token->type));
 
         switch (token->type) {
             case NUMBER:
@@ -30,9 +25,22 @@ void print_tokens(Array tokens) {
             case PLUS:
                 printf(" | Value: %c\n", *(char *)token->value);
                 break;
+            case SHOW:
+                printf(" | Value: %s\n", (char *)token->value);
+                break;
             default:
-                printf(" | Not Implemented in `print_tokens`.\n");
+                printf(" | Not Implemented `%s` in `print_tokens`.\n", token_type_to_str(token->type));
                 break;
         }
 	}
+}
+
+const char *token_type_to_str(enum TokenType type) {
+    const char *types[] = {
+        "NUMBER",
+        "PLUS",
+        "SHOW"
+    };
+
+    return types[type];
 }
