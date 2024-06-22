0f2ec4060700aca9b156c32c3266a59ee292f7f7
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-22 10:35:00 -0300

    feat: Add basic parser functionality
    - Added `parser()` function to parse the token stream.
    - Implemented basic arithmetic operations (PLUS).
    - Implemented `SHOW` operation to display the top value on the stack.
    - Added error handling for insufficient operands.
    - Introduced the `error()` function to display error messages with location information.

--- src/main.c
+++ src/main.c
@@ -3,10 +3,12 @@
 #include "lexer.h"
 #include "dynamic_array.h"
 #include "tokens.h"
+#include "error.h"
 
 #define MAX_LINE_LENGTH 128
 
 char *readFile(char *filepath);
+void parser(Array tokens);
 
 void runProgram(Lexer *lexer) {
 	lexer->file = "REPL";
@@ -34,86 +36,9 @@ void runProgram(Lexer *lexer) {
 		lexer->line = 1;
 		lexer->col = 1;
 
-		scan(lexer);
+		Array tokens = scan(lexer);
+		parser(tokens);
 	}
-}
-
-void parser(Array tokens) {
-	Array stack;
-	array_init(&stack, sizeof(Token));
-
-	// print_tokens(tokens);
-
-	for (int i=0; i<tokens.length; i++) {
-		Token *token = &((Token *)tokens.array)[i];
-
-		if (token->type == NUMBER) {
-			Token tk;
-            tk.type = NUMBER;
-            tk.value = malloc(sizeof(int));
-            *(int *)tk.value = *(int *)token->value;
-            tk.loc = token->loc;
-
-            array_push(&stack, &tk);
-
-		} else if (token->type == PLUS) {
-			// ERROR
-        	if (stack.length < 2) {
-                fprintf(stderr, "Not enough operands in the stack for PLUS operation.\n");
-
-                tokens_array_cleanup(&tokens);
-				tokens_array_cleanup(&stack);
-
-                exit(EXIT_FAILURE);
-            }
-
-            Token b;
-            array_pop(&stack, &b);
-
-            Token a;
-            array_pop(&stack, &a);
-
-            Token tk;
-            tk.type = NUMBER;
-            tk.value = malloc(sizeof(int));
-            *(int *)tk.value = *(int *)a.value + *(int *)b.value;
-            tk.loc = token->loc;
-
-            array_push(&stack, &tk);
-
-            free(a.value);
-            free(b.value);
-
-        } else if (token->type == SHOW) {
-        	// ERROR
-        	if (stack.length < 1) {
-                fprintf(stderr, "Not enough operands in the stack for SHOW operation.\n");
-
-                tokens_array_cleanup(&tokens);
-				tokens_array_cleanup(&stack);
-
-                exit(EXIT_FAILURE);
-            }
-
-            Token value;
-            array_pop(&stack, &value);
-
-            if (value.type == NUMBER) {
-            	printf("%d\n", *(int *)value.value);
-
-            } else {
-            	printf("Not implemented type `%s` for `SHOW` operation.\n", token_type_to_str(token->type));
-            }
-
-		} else {
-			printf("Not Implemented in `parser`.\n");
-		}
-	}
-
-	print_tokens(stack);
-
-	tokens_array_cleanup(&tokens);
-	tokens_array_cleanup(&stack);
 }
 
 void runFile(Lexer *lexer, char *file) {
@@ -171,4 +96,76 @@ char *readFile(char *filepath) {
 	fclose(fp);
 
 	return buffer;
+}
+
+void parser(Array tokens) {
+	Array stack;
+	array_init(&stack, sizeof(Token));
+
+	for (int i=0; i<tokens.length; i++) {
+		Token *token = &((Token *)tokens.array)[i];
+
+		if (token->type == NUMBER) {
+			Token tk;
+            tk.type = NUMBER;
+            tk.value = malloc(sizeof(int));
+            *(int *)tk.value = *(int *)token->value;
+            tk.loc = token->loc;
+
+            array_push(&stack, &tk);
+
+		} else if (token->type == PLUS) {
+			// ERROR
+        	if (stack.length < 2) {
+        		error("Not enough operands in the stack for PLUS operation.", token->loc);
+
+                tokens_array_cleanup(&tokens);
+				tokens_array_cleanup(&stack);
+            }
+
+            Token b;
+            array_pop(&stack, &b);
+
+            Token a;
+            array_pop(&stack, &a);
+
+            Token tk;
+            tk.type = NUMBER;
+            tk.value = malloc(sizeof(int));
+            *(int *)tk.value = *(int *)a.value + *(int *)b.value;
+            tk.loc = token->loc;
+
+            array_push(&stack, &tk);
+
+            free(a.value);
+            free(b.value);
+
+        } else if (token->type == SHOW) {
+        	// ERROR
+        	if (stack.length < 1) {
+                error("Not enough operands in the stack for SHOW operation.", token->loc);
+
+                tokens_array_cleanup(&tokens);
+				tokens_array_cleanup(&stack);
+            }
+
+            Token value;
+            array_pop(&stack, &value);
+
+            if (value.type == NUMBER) {
+            	printf("%d\n", *(int *)value.value);
+
+            } else {
+            	printf("Not implemented type `%s` for `SHOW` operation.\n", token_type_to_str(token->type));
+            }
+
+		} else {
+			printf("Not Implemented in `parser`.\n");
+		}
+	}
+
+	print_tokens(stack);
+
+	tokens_array_cleanup(&tokens);
+	tokens_array_cleanup(&stack);
 }
