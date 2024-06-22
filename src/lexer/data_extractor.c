7abb66cba3c23b4be181ccf6113e59c71fd4d4fa
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-22 00:25:23 -0300

    feat: Add identifier extraction
    This commit adds functionality to the lexer to extract identifiers.
    
    - Added `isAlphaNumeric` function to check for alphanumeric characters.
    - Added `extractIdentifier` function to extract identifiers from the input stream.
    - Updated the lexer to use the `extractIdentifier` function when encountering an identifier.
    
    This change improves the lexer's ability to parse code by allowing it to recognize and extract identifiers, which are essential components of most programming languages.

--- src/lexer/data_extractor.c
+++ src/lexer/data_extractor.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include "data_extractor.h"
 
+int isNumber(char ch) {
+	return (ch >= '0' && ch <= '9');
+}
+
+int isAlpha(char ch) {
+	return (ch >= 'a' && ch <= 'z') ||
+		   (ch >= 'A' && ch <= 'Z') ||
+		   (ch == '_');
+}
+
+int isAlphaNumeric(char ch) {
+	return isNumber(ch) || isAlpha(ch);
+}
+
 int extractNumber(Lexer *lexer) {
 	char buffer[17];
 	int buffer_idx = 0;
@@ -41,4 +55,33 @@ int extractNumber(Lexer *lexer) {
 	}
 
 	return value;
+}
+
+char *extractIdentifier(Lexer *lexer) {
+    int buffer_size = 32;
+    char *buffer = (char *)malloc(buffer_size * sizeof(char));
+    int buffer_idx = 0;
+    char ch;
+
+    while (1) {
+        ch = peek(lexer, 0);
+
+        if (!isAlphaNumeric(ch)) {
+            break;
+        }
+
+        if (buffer_idx >= buffer_size - 1) {
+            buffer_size *= 2;
+            char *new_buffer = realloc(buffer, buffer_size * sizeof(char));
+            buffer = new_buffer;
+        }
+
+        buffer[buffer_idx] = ch;
+        buffer_idx++;
+        consume(lexer);
+    }
+
+    buffer[buffer_idx] = '\0';
+
+    return buffer;
 }
