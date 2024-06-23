86a69f26914359a2dc1667f016765869d4771799
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-06-22 23:24:39 -0300

    feat: Add lexer utilities
    - Added `lexer_utils.h` file containing the declaration of functions for creating tokens.
    - Added `lexer_utils.c` file containing the implementation of functions for creating tokens.
    - Added functions for creating tokens of type char, int and string.
    - All functions accept a location, a token type and a value.
    - The functions create a new token and allocate memory for the value.
    - The functions copy the value to the allocated memory.
    - The functions set the type and location of the token.

---
+++ include/lexer_utils.h
@@ -0,0 +1,10 @@
+#ifndef LEXER_UTILS_H
+#define LEXER_UTILS_H
+
+#include "tokens.h"
+
+Token create_token_char(const Loc location, const TokenType type, const char value);
+Token create_token_int(const Loc location, const TokenType type, const int value);
+Token create_token_string(const Loc location, const TokenType type, const char *value);
+
+#endif  // LEXER_UTILS_H
---
+++ src/lexer/lexer_utils.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lexer_utils.h"
+
+Token create_token_char(const Loc location, const TokenType type, const char value) {
+	Token token;
+	token.type = type;
+	token.value = malloc(sizeof(char));
+	*(char *)token.value = value;
+	token.loc = location;
+
+	return token;
+}
+
+Token create_token_int(const Loc location, const TokenType type, const int value) {
+	Token token;
+	token.type = type;
+	token.value = malloc(sizeof(int));
+	*(int *)token.value = value;
+	token.loc = location;
+
+	return token;
+}
+
+Token create_token_string(const Loc location, const TokenType type, const char *value) {
+	int value_len = strlen(value);
+
+	Token token;
+	token.type = type;
+	token.value = malloc(sizeof(char) * value_len + 1);
+	strcpy(token.value, value);
+	token.loc = location;
+
+	return token;
+}
